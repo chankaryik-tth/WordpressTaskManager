6974483e2e34a60ed145503bd95da5fc2b42e6b6
THENICEONE\Daniel <daniel.yap@thetechyhub.com>
2018-03-15 19:08:50 +0800

    Modified acf-related-custom-function.php to allow update of acf fields

--- wp-content/acf-related-custom-function.php
+++ wp-content/acf-related-custom-function.php
@@ -2,14 +2,25 @@
 define('WP_USE_THEMES', false);
 	require('../wp-load.php');
 
-if (isset($_GET['id'])&&isset($_GET['desc'])&&isset($_GET['deadline'])){
+	$request = $_GET['method'];
+if($request == "POST"){
+	if (isset($_GET['id'])&&isset($_GET['desc'])&&isset($_GET['deadline'])){
 	
-	$id = $_GET['id'];
-	$desc = $_GET['desc'];
-	$deadline = $_GET['deadline'];
+		$id = $_GET['id'];
+		$desc = $_GET['desc'];
+		$deadline = $_GET['deadline'];
 
-	update_field('description', $desc, $id);
-	update_field('deadline', $deadline, $id );
+		update_field('description', $desc, $id);
+		update_field('deadline', $deadline, $id );
+	}
+}elseif ($request == "PUT") {
+	if (isset($_GET['id'])&&isset($_GET['key'])&&isset($_GET['value'])){
+
+		$id = $_GET['id'];
+		$key = $_GET['key'];
+		$value = $_GET['value'];
+		
+		update_field($key, $value, $id );
+	}
 }
-
  ?>
